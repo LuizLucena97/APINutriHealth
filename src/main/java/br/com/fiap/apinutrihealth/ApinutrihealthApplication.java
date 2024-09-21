a7cc9b08f536b4a72a6f9247a2865c74013b85cd
LuizLucena97 <luiz.lucena97@gmail.com>
2024-09-21 11:07:55 -0300

    config project

---
+++ src/main/java/br/com/fiap/apinutrihealth/ApinutrihealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apinutrihealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApinutrihealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApinutrihealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/apinutrihealth/ApinutrihealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.apinutrihealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApinutrihealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
